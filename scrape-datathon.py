25f35233b17c98b468799b7d106fdf30e5fa9cc7
albiebrown <albert_brown@brown.edu>
2017-03-03 15:40:14 -0500

    Added csv export

--- scrape-datathon.py
+++ scrape-datathon.py
@@ -9,8 +9,26 @@ url = "http://bds.cs.brown.edu/datathon/"
 
 soup = BeautifulSoup(urlopen(url).read(), 'lxml')
 
-rows = soup.find_all("div", { "class" : "workshop-row" })	
+rows = soup.find_all("div", { "class" : "workshop-row" })
 
-for row in rows:
-	print row
-	print "#####"
+with open("datathon-scrape.csv", 'wb') as outfile:
+	writer = csv.writer(outfile)
+	writer.writerow(["Name", "Date", "Description"])
+
+	for row in rows:
+
+		# Find name
+		name = row.find("h4")
+		if name:
+			name = name.decode_contents(formatter="html")
+			print name
+
+		paragraphs = row.find_all("p")
+		if len(paragraphs) > 1:
+			date = paragraphs[0].decode_contents(formatter="html")
+			description = paragraphs[1].decode_contents(formatter="html")
+			print date
+			print description
+
+		if name or len(paragraphs) > 0:
+			writer.writerow([name, date, description])
